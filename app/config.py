6f4fe0a6bce3ae27899e12ae1864e0bc72a23932
chava28pvp <129213697+chava28pvp@users.noreply.github.com>
2025-06-03 17:55:34 -0600

    Remove config.py from Git and add it to .gitignore

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'devkey123')  # Usar variable de entorno en producción
-    SQLALCHEMY_DATABASE_URI = (
- "mssql+pyodbc://@GAMEZ28\\SQLEXPRESS/ToDoListConnection"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
-    "&trusted_connection=yes"    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
